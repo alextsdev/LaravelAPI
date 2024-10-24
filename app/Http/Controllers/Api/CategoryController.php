25cdc6e013d9db23aa4978d3257d133657597efc
Alex_dev <131791659+SpeedAlex150@users.noreply.github.com>
2024-10-24 17:10:16 +0200

    Añadir restricciones para mostrar unicamente lo que le pongamos en CategoryResource

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        return CategoryResource::class::collection(Category::all());
+    }
+
+    public function show(Category $category)
+    {
+        return new CategoryResource($category);
+    }
+}
