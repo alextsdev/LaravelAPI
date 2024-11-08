0297249c03f42163ce4b3fbb838b7a9934dbe18c
Alex_dev <131791659+SpeedAlex150@users.noreply.github.com>
2024-11-08 15:57:42 +0100

    Activamos el middelware para autenticar rutas

--- routes/api.php
+++ routes/api.php
@@ -11,11 +11,9 @@ Route::get('/user', function (Request $request) {
 
 Route::get('list/categories', [categoryController::class, 'list']);
 
-//Route::get('categories', [CategoryController::class, 'index']);
-//Route::get('categories/{category}', [CategoryController::class, 'show']);
-//Route::post('categories', [CategoryController::class, 'store']);
-//Route::put('categories/{category}', [CategoryController::class, 'update']);
-//Route::delete('categories/{category}', [CategoryController::class, 'destroy']);
-Route::apiResource('categories', CategoryController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('categories', CategoryController::class);
+    Route::get('products', [ProductController::class, 'index']);
+});
 
-Route::get('products', [ProductController::class, 'index']);
+
