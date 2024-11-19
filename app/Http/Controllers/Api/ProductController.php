20f2a7d1973cabf7dcf281c73432315032bfb007
Alex_dev <131791659+SpeedAlex150@users.noreply.github.com>
2024-11-19 15:56:19 +0100

    Distintas formas de documentar

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -5,7 +5,11 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
+use Knuckles\Scribe\Attributes\Group;
+use Knuckles\Scribe\Attributes\QueryParam;
 
+#[Group('Products', description: 'Managing products')]
+#[QueryParam('page', type: 'int', description: 'The page number. Defaults to 1.')]
 class ProductController extends Controller
 {
     public function index()
